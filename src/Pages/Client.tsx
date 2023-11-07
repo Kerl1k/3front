e5640b70f75eb4de597c8118707f990f79614672
Kirill <Kirill@Techno-k340.glavapu.local>
2023-11-07 14:05:22 +0300

    func client

--- src/Pages/Client.tsx
+++ src/Pages/Client.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import MyModal from "../components/modal/MyModal";
 import ClientModal from "../components/ClientModal";
+import MyButton from "../components/button/MyButton";
 
 interface Form {
   name: string;
@@ -11,6 +12,24 @@ interface Form {
 }
 
 const Client = () => {
+  const [filterr, setFilterr] = useState("");
+  const [qwe, setQwe] = useState([
+    {
+      name: "qew",
+      scale: "123",
+      color: "RED",
+      url: "https",
+      status: "waiting",
+    },
+    {
+      name: "qew",
+      scale: "123",
+      color: "RED",
+      url: "https",
+      status: "waiting",
+    },
+  ]);
+
   const { register, handleSubmit } = useForm<Form>({
     defaultValues: {
       name: "",
@@ -19,28 +38,39 @@ const Client = () => {
       url: "",
     },
   });
-  const [modal, setModal] = useState(false)
-  const [info, setInfo] = useState({})
+  const [modal, setModal] = useState(false);
+  const [info, setInfo] = useState({});
 
   const submit: SubmitHandler<Form> = (data) => {
     setInfo(data);
+  };
+
+  const filter = (value: string) => {
+    console.log(value);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit(submit)}>
         <input type="text" {...register("name")} />
-        <input type="radio" value={3000} {...register("scale")} />3000
-        <input type="radio" value={2000} {...register("scale")} />2000
-        <input type="radio" value={1000} {...register("scale")} />1000
-        <input type="radio" value={500} {...register("scale")} />500
+        <input type="radio" value={3000} {...register("scale")} />
+        3000
+        <input type="radio" value={2000} {...register("scale")} />
+        2000
+        <input type="radio" value={1000} {...register("scale")} />
+        1000
+        <input type="radio" value={500} {...register("scale")} />
+        500
         <input type="text" {...register("color")} />
         <input type="text" {...register("url")} />
-        <button onClick={()=> setModal(true)}>Отправить</button>
+        <button onClick={() => setModal(true)}>Отправить</button>
       </form>
-
+      <div>
+        <input onChange={(e) => setFilterr(e.target.value)} type="text" />
+        <MyButton onClick={() => filter(filterr)}>Посмотреть</MyButton>
+      </div>
       <MyModal visible={modal} setVisible={setModal}>
-        <ClientModal info={info}/>
+        <ClientModal info={info} />
       </MyModal>
     </div>
   );
